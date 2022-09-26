86258475c99d4aa1d1050334498079c317dc60ee
Yousefq80 <114352485+Yousefq80@users.noreply.github.com>
2022-09-26 20:03:41 +0300

    Yousef Alharby
    Task has been solved

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 # Create a dictionary called `person`, it has three properties, `name` which is a string, `age` which is an integer, `hobbies` which is a list of strings.
-
+person={"name":"Yousef","age":40,
+"hobbies":["reding","playing fotball"],
+}
 
 # Create a function called `change_age` which takes the `person` dictionary and a `number` as arguments, this function changes the age in the dictionary to the `number`. The return value of this function is the updated dictionary.
 
 
-def change_age(person, number):
-    ...
-
-
+def change_age(person,number):
+   person['age'] = number
+   return person
 print(change_age(person, 21))
 
 
@@ -15,9 +16,8 @@ print(change_age(person, 21))
 
 
 def add_hobby(person, hobby):
-    ...
-
-
+    person["hobbies"].append(hobby)
+    return person
 print(add_hobby(person, "Table Tennis"))
 
 
@@ -27,11 +27,35 @@ print(add_hobby(person, "Table Tennis"))
 
 
 def add_job(person, job):
-    ...
+#    for key in person:
+#     if key.find("job"):
+#         print(person)
+#     else:
+#         person.__setitem__("job","IT")
+#     return person
+  if "job" not in person:
+    person["job"]="eng"
+  else:
+    print("the key is already there")
+  return person
+print(add_job(person,"job"))
+
+
+
 
 
 # 2. Create a function called `check_hobbies`, which takes a person dictionary as an argument. This function checks the number of hobbies for this person, if it's more than three hobbies, print to the user a message telling them that they're talented.
 
 
+
 def check_hobbies(person):
-    ...
+    x=0
+    for hobby in person["hobbies"]:
+        x=+x
+    if x >= 3:
+        print("you are talent")
+    else:
+        print("add one more")
+        
+print(check_hobbies(person))
+
